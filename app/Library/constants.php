f4a77bb59ce5f776d0c2bf5ef9c3f07a4dca93b5
CSN\admin <tt.hau94@gmail.com>
2019-11-19 10:44:29 +0700

    no message

---
+++ app/Library/constants.php
@@ -0,0 +1,4 @@
+<?php
+
+define('MUSIC_DATA_PATH', 'data/128/');
+define('SUB_ALLBUM', '/nghe-album/');
---
+++ app/Providers/SolariumServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Solarium\Client;
+
+class SolariumServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return new Client($app['config']['solarium']);
+        });
+    }
+    public function provides()
+    {
+        return [Client::class];
+    }
+}
