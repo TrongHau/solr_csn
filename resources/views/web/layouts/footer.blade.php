0ec22d635f3f6901e41ef4cc940825b303d95f05
CSN\admin <tt.hau94@gmail.com>
2019-12-01 01:02:13 +0700

    no message

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -8,6 +8,7 @@
                 delete $.ajaxSettings.headers["X-CSRF-TOKEN"];
                 $.ajax( {
                     url: "<?php echo env('SEARCH_URL'); ?>/search/real",
+
                     // url: window.location.origin + "/search/real",
                     dataType: "json",
                     data: {
@@ -192,7 +193,8 @@
         return '';
     }
     function redirectSearch() {
-        window.location.href = "/tim-kiem?q=" + $('#search_autocomplete').val();
+        window.location.href = "<?php echo env('SEARCH_URL'); ?>/tim-kiem?q=" + $('#search_autocomplete').val();
+        
     }
 </script>
 <!-- Modal -->
