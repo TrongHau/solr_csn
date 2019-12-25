dbc60b2a0490fc71d2f9ca5f736e2f47d4de9553
CSN\admin <tt.hau94@gmail.com>
2019-12-25 10:49:26 +0700

    no message

--- resources/views/vendor/backpack/upload_artist/edit.blade.php
+++ resources/views/vendor/backpack/upload_artist/edit.blade.php
@@ -107,11 +107,11 @@ use App\Library\Helpers;
                                 </div>
                                 <div class="form-group col-xs-4">
                                     <label style="display: -webkit-box;">Avatar</label>
-                                    <img class="mr-3" style="width: 100%"id="artist_avatar_uploaded" src="{{$artist_exists['artist_avatar'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], PUBLIC_AVATAR_ARTIST_PATH, true).$artist_exists['artist_avatar'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                    <img class="mr-3" style="width: 100%"id="artist_avatar_uploaded" src="{{$artist_exists['artist_avatar'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], AVATAR_ARTIST_CROP_PATH, true).$artist_exists['artist_avatar'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
                                 </div>
                                 <div class="form-group col-xs-9" style="margin-top: 50px;">
                                     <label style="display: -webkit-box;">Cover</label>
-                                    <img class="mr-3" style="width: 100%" id="artist_cover_uploaded" src="{{$artist_exists['artist_cover'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], PUBLIC_COVER_ARTIST_PATH, true).$artist_exists['artist_cover'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                    <img class="mr-3" style="width: 100%" id="artist_cover_uploaded" src="{{$artist_exists['artist_cover'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], COVER_ARTIST_CROP_PATH, true).$artist_exists['artist_cover'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
                                 </div>
                             </div>
                         @endif
