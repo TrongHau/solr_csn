ad09143e39aef9193147ab6675b955845d292884
CSN\admin <tt.hau94@gmail.com>
2019-12-25 10:42:00 +0700

    no message

--- resources/views/vendor/backpack/upload_artist/edit.blade.php
+++ resources/views/vendor/backpack/upload_artist/edit.blade.php
@@ -41,26 +41,26 @@ use App\Library\Helpers;
                     <div class="box-header with-border">
                         <div class="form-group col-xs-6">
                         @if ($crud->model->translationEnabled())
-                        <!-- Single button -->
-                            <div class="btn-group pull-right">
-                                <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    @foreach ($crud->model->getAvailableLocales() as $key => $locale)
-                                        <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            <h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
-                        @else
-                            <h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
-                        @endif
+                            <!-- Single button -->
+                                <div class="btn-group pull-right">
+                                    <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        @foreach ($crud->model->getAvailableLocales() as $key => $locale)
+                                            <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                <h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
+                            @else
+                                <h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
+                            @endif
                         </div>
                         @if($fields['type']['value'] == 0)
-                        <div class="form-group col-xs-6">
-                            <h3 class="box-title">Thông tin ca sĩ cũ</h3>
-                        </div>
+                            <div class="form-group col-xs-6">
+                                <h3 class="box-title">Thông tin ca sĩ cũ</h3>
+                            </div>
                         @endif
                     </div>
                     <div class="box-body row display-flex-wrap" style="display: flex;flex-wrap: wrap;">
@@ -73,7 +73,7 @@ use App\Library\Helpers;
                             @endif
                             <div class="form-group col-xs-12">
                                 <label style="display: -webkit-box;">Avatar</label>
-                                <img class="mr-3" width="152px" height="152px" id="artist_avatar_uploaded" src="{{$fields['artist_avatar']['value'] ? Helpers::file_path($fields['id']['value'], PUBLIC_CACHE_AVATAR_ARTIST_PATH, true).$fields['artist_avatar']['value'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                <img class="mr-3" width="152px" height="152px" id="artist_avatar_uploaded" src="{{$fields['artist_avatar']['value'] ? env('UPLOAD_TEMPLATE_URL').Helpers::file_path($fields['id']['value'], PUBLIC_CACHE_AVATAR_ARTIST_PATH, true).$fields['artist_avatar']['value'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
                                 <div class="media-body">
                                     <div class="form-group" style="margin-top: 10px;">
                                         <input type="file" class="form-control-file" name="choose_artist_avatar" id="choose_artist_avatar">
@@ -83,7 +83,7 @@ use App\Library\Helpers;
 
                             <div class="form-group col-xs-12">
                                 <label style="display: -webkit-box;">Cover</label>
-                                <img class="mr-3" width="350px" height="90x" id="artist_cover_uploaded" src="{{$fields['artist_cover']['value'] ? Helpers::file_path($fields['id']['value'], PUBLIC_CACHE_COVER_ARTIST_PATH, true).$fields['artist_cover']['value'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                <img class="mr-3" width="350px" height="90x" id="artist_cover_uploaded" src="{{$fields['artist_cover']['value'] ? env('UPLOAD_TEMPLATE_URL').Helpers::file_path($fields['id']['value'], PUBLIC_CACHE_COVER_ARTIST_PATH, true).$fields['artist_cover']['value'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
                                 <div class="media-body">
                                     <div class="form-group" style="margin-top: 10px;">
                                         <input type="file" class="form-control-file" name="choose_artist_cover" id="choose_artist_cover">
@@ -92,28 +92,28 @@ use App\Library\Helpers;
                             </div>
                         </div>
                         @if($fields['type']['value'] == 0)
-                        <div class="form-group col-xs-6">
-                            <div class="form-group col-xs-12">
-                                <label>Nghệ Danh</label>
-                                <input disabled type="text" value="{{$artist_exists['artist_nickname']}}" class="form-control">
+                            <div class="form-group col-xs-6">
+                                <div class="form-group col-xs-12">
+                                    <label>Nghệ Danh</label>
+                                    <input disabled type="text" value="{{$artist_exists['artist_nickname']}}" class="form-control">
+                                </div>
+                                <div class="form-group col-xs-12">
+                                    <label>Giới Tính</label>
+                                    <input disabled type="text" value="{{$artist_exists['artist_gender'] == 0 ? 'Nam' : 'Nữ'}}" class="form-control">
+                                </div>
+                                <div class="form-group col-xs-12">
+                                    <label>Ngày sinh</label>
+                                    <input disabled type="text" value="{{date("d/m/Y", strtotime($artist_exists['artist_birthday']))}}" class="form-control">
+                                </div>
+                                <div class="form-group col-xs-4">
+                                    <label style="display: -webkit-box;">Avatar</label>
+                                    <img class="mr-3" style="width: 100%"id="artist_avatar_uploaded" src="{{$artist_exists['artist_avatar'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], PUBLIC_AVATAR_ARTIST_PATH, true).$artist_exists['artist_avatar'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                </div>
+                                <div class="form-group col-xs-9" style="margin-top: 50px;">
+                                    <label style="display: -webkit-box;">Cover</label>
+                                    <img class="mr-3" style="width: 100%" id="artist_cover_uploaded" src="{{$artist_exists['artist_cover'] ? env('DATA_URL').Helpers::file_path($artist_exists['artist_id'], PUBLIC_COVER_ARTIST_PATH, true).$artist_exists['artist_cover'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
+                                </div>
                             </div>
-                            <div class="form-group col-xs-12">
-                                <label>Giới Tính</label>
-                                <input disabled type="text" value="{{$artist_exists['artist_gender'] == 0 ? 'Nam' : 'Nữ'}}" class="form-control">
-                            </div>
-                            <div class="form-group col-xs-12">
-                                <label>Ngày sinh</label>
-                                <input disabled type="text" value="{{date("d/m/Y", strtotime($artist_exists['artist_birthday']))}}" class="form-control">
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label style="display: -webkit-box;">Avatar</label>
-                                <img class="mr-3" style="width: 100%"id="artist_avatar_uploaded" src="{{$artist_exists['artist_avatar'] ? Helpers::file_path($artist_exists['artist_id'], PUBLIC_AVATAR_ARTIST_PATH, true).$artist_exists['artist_avatar'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
-                            </div>
-                            <div class="form-group col-xs-9" style="margin-top: 50px;">
-                                <label style="display: -webkit-box;">Cover</label>
-                                <img class="mr-3" style="width: 100%" id="artist_cover_uploaded" src="{{$artist_exists['artist_cover'] ? Helpers::file_path($artist_exists['artist_id'], PUBLIC_COVER_ARTIST_PATH, true).$artist_exists['artist_cover'].'?time='.time() : '/imgs/avatar_default.png'}}" alt="">
-                            </div>
-                        </div>
                         @endif
                     </div><!-- /.box-body -->
 
@@ -129,7 +129,7 @@ use App\Library\Helpers;
                             @endif
                             <a href="{{ $crud->hasAccess('list') ? url($crud->route).'/preview/'.$fields['id']['value'].'' : url()->previous() }}" target="_blank" class="btn btn-warning"><span class="fa fa-align-center"></span> &nbsp;Xem Trước</a>
                             @if($crud->hasAccess('delete'))
-                            <a href="javascript:void(0)" onclick="deleteEntry({{$fields['id']['value']}})" class="btn btn-default"><span class="fa fa-trash"></span> &nbsp;Xóa</a>
+                                <a href="javascript:void(0)" onclick="deleteEntry({{$fields['id']['value']}})" class="btn btn-default"><span class="fa fa-trash"></span> &nbsp;Xóa</a>
                             @endif
                             <div class="btn-group">
                                 <button type="submit" class="btn btn-success">
