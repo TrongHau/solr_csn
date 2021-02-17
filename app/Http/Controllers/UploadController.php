40d5b20a2ac4111049231a858b2ba8f4228269de
CSN\admin <tt.hau94@gmail.com>
2021-02-17 12:04:05 +0700

    no message

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -87,6 +87,9 @@ class UploadController extends Controller
         return view('upload.upload_artist');
     }
     public function createMusic(Request $request, $musicId = null) {
+        if(!Auth::user()->username) {
+            return redirect('/user/' . Auth::user()->id . '?edit_profile=true');
+        }
         $typeUpload = 'music';
         $uploadExp = '';
         if($musicId) {
@@ -125,6 +128,9 @@ class UploadController extends Controller
         return view('upload.upload_music', compact('typeUpload', 'music', 'album', 'userMusic', 'uploadExp'));
     }
     public function createVideo(Request $request, $musicId = null) {
+        if(!Auth::user()->username) {
+            return redirect('/user/' . Auth::user()->id . '?edit_profile=true');
+        }
         $typeUpload = 'video';
         $uploadExp = '';
         if($musicId) {
@@ -161,6 +167,9 @@ class UploadController extends Controller
         return view('upload.upload_music', compact('typeUpload', 'music', 'album', 'userMusic', 'uploadExp'));
     }
     public function createAlbum(Request $request, $coverId = null) {
+        if(!Auth::user()->username) {
+            return redirect('/user/' . Auth::user()->id . '?edit_profile=true');
+        }
         if($coverId) {
             if(Auth::user()->hasPermission('duyet_sua_nhac')) {
                 $album = $this->coverRepository->findCover($coverId);
